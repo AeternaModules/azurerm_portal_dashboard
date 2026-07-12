3f0e0017569a3f6ff70d1c136fd866fe29a698ed
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "portal_dashboards_id" {
+  description = "Map of id values across all portal_dashboards, keyed the same as var.portal_dashboards"
+  value       = { for k, v in azurerm_portal_dashboard.portal_dashboards : k => v.id }
+}
 output "portal_dashboards_dashboard_properties" {
   description = "Map of dashboard_properties values across all portal_dashboards, keyed the same as var.portal_dashboards"
   value       = { for k, v in azurerm_portal_dashboard.portal_dashboards : k => v.dashboard_properties }
